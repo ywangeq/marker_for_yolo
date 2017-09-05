8205d32e7f755d81131b3956266474b2279d74b9
Yuan WANG <ywangeq@connect.ust.hk>
2017-09-05 15:51:02 +0800

    Update 'src/marker.cpp'

--- src/marker.cpp
+++ src/marker.cpp
@@ -16,7 +16,7 @@ ros::Publisher marker_pub;
 int num=0;
 void markercallback(const boundingbox_msgs::Boundingbox& msg){
    
-   ros::Rate r(30);
+   ros::Rate r(10);
    
 
 
@@ -48,19 +48,18 @@ void markercallback(const boundingbox_msgs::Boundingbox& msg){
           
               double width=msg.w;
               double  d=(0.0397*0.5)/(width*0.00007);
-                          std::cout<<"11"<<"distance  "<<d<<"\n";
-                          std::cout<<"11"<<"width  "<<width<<"\n";
+                          
               marker.action=visualization_msgs::Marker::ADD;
-              marker.pose.position.x=d*0.3;
-              marker.pose.position.y=d*0.7;
+              marker.pose.position.x=0.00007*(msg.x)*d/0.0397;
+              marker.pose.position.y=0.00007*(msg.y)*d/0.0397;
               marker.pose.position.z = 0;
               marker.pose.orientation.x = 0.0;
               marker.pose.orientation.y = 0.0;
               marker.pose.orientation.z = 0.0;
               marker.pose.orientation.w = 1.0;
-              marker.scale.x = 0.1;
-              marker.scale.y = 0.1;
-              marker.scale.z = 0.1;
+              marker.scale.x =0.5;
+              marker.scale.y =0.5;
+              marker.scale.z =0.5;
  
 
               marker.color.r = 0.0f;
@@ -68,13 +67,13 @@ void markercallback(const boundingbox_msgs::Boundingbox& msg){
               marker.color.b = 0.0f;
               marker.color.a = 1.0;
  
-              marker.lifetime = ros::Duration(1);
+              marker.lifetime = ros::Duration(0.5);
 
               ma->markers.push_back(marker);
 
              
-}/*else if(msg.objectName=="chair"){
-               for(int num=0;num<5;num++){ 
+}else if(msg.objectName=="chair"){
+              
               visualization_msgs::Marker marker;
          
               marker.header.frame_id="usb_cam";
@@ -82,38 +81,41 @@ void markercallback(const boundingbox_msgs::Boundingbox& msg){
               marker.ns="chair";
               marker.id=num;
               double width=msg.w;
-              int  d=(0.0397*0.5)/(width*0.00007);
+
+              double  d=(0.0397*0.8)/(width*0.00007);
+                          
               marker.type = visualization_msgs::Marker::CUBE;
 
               marker.action=visualization_msgs::Marker::ADD;
-              marker.pose.position.x=d*0.4;
-              marker.pose.position.y=d*0.2;
+              marker.pose.position.x=0.00007*(msg.x)*d/0.0397;
+              marker.pose.position.y=0.00007*(msg.y)*d/0.0397;
               marker.pose.position.z = 0;
               marker.pose.orientation.x = 0.0;
               marker.pose.orientation.y = 0.0;
               marker.pose.orientation.z = 0.0;
               marker.pose.orientation.w = 1.0;
-              marker.scale.x = 0.1;
-              marker.scale.y = 0.1;
-              marker.scale.z = 0.1;
- 
+              marker.scale.x = 0.5;
+              marker.scale.y = 0.5;
+              marker.scale.z = 0.5;
+
 
               marker.color.r = 1.0f;
               marker.color.g = 0.0f;
               marker.color.b = 0.0f;
               marker.color.a = 1.0;
  
-             marker.lifetime = ros::Duration(10);
+             marker.lifetime = ros::Duration(0.5);
           
-            std::cout<<"22222222"<<"\n";
+            std::cout<<"x"<<0.00007*(msg.x)*d/0.0397<<"\n";
+             std::cout<<"y"<<0.00007*(msg.y)*d/0.0397<<"\n";
               ma->markers.push_back(marker);
                // marker_pub.publish(marker);
           
 
-   }      
+    
 
 
-}*/
+}
 
 
 
